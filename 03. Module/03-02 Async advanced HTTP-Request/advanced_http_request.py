28bf1a5f101d0dd7ca1b1788be89afc2303035a4
SDE <deanisik@gmail.com>
2025-07-17 10:00:40 +0700

    3st module. Advanced async http-request

--- 03. Module/03-02 Async advanced HTTP-Request/advanced_http_request.py
+++ 03. Module/03-02 Async advanced HTTP-Request/advanced_http_request.py
@@ -1,4 +1,4 @@
-'''
+"""
 Задача - Асинхронный HTTP-запрос. Продвинутая реализация.
 Напишите асинхронную функцию fetch_urls, которая принимает файл со списком урлов (каждый URL адрес возвращает JSON)
 и сохраняет результаты выполнения в другой файл (result.jsonl), где ключами являются URL,
@@ -12,52 +12,82 @@
 Урлов в файле может быть десятки тысяч
 Некоторые урлы могут весить до 300-500 мегабайт
 При внезапной остановке и/или перезапуске скрипта - допустимо скачивание урлов по новой.
-'''
+"""
 
+import os
 import json
 import aiohttp
 import asyncio
 from typing import Any
-from aiohttp import ClientSession
+from aiohttp import ClientSession, ClientError
 
 semaphore = asyncio.Semaphore(5)
 
 
-def reads_urls(file_path: str) -> list[str]:
+async def read_urls(file_path: str) -> list[str]:
     with open(file_path, "r", encoding="utf-8") as f:
-       return [line for line in f]
+        return [line for line in f]
 
 
-def save_file(file_path: str, result: dict[str: Any]) -> None:
+async def save_results(result: list, file_path: str):
     with open(file_path, "w", encoding="utf-8") as f:
         for res in result:
-            f.write(json.dumps(res, ensure_ascii=False) + "\n")
+            f.write(json.dumps(res, ensure_ascii=False))
 
 
 async def fetch_url(session: ClientSession, url: str) -> (str, dict[str, list]):
-    async with session.get(url) as response:
-        return url, await response.json()
+    try:
+        async with session.get(
+            url,
+            headers={
+                "Content-Type": "application/json",
+                "Accept": "application/json",
+            },
+        ) as response:
+            content = {
+                "status": response.status,
+                "headers": dict(response.headers),
+                "content_type": response.content_type,
+                # 'text': await response.text(),
+                "url": str(response.url),
+                "history": [str(r.url) for r in response.history],
+                "version": response.version,
+            }
+            return {
+                "url": url,
+                "content": content,
+            }
+    except (ClientError, asyncio.TimeoutError, json.JSONDecodeError) as e:
+        return {
+            "url": url,
+            "error": str(e),
+        }
 
 
-async def fetch_urls(urls: list[str]) -> dict[str, Any]:
-    results = {}
+async def fetch_urls(in_file: str, out_file: str) -> list[dict[str, Any]]:
+    urls = await read_urls(in_file)
+    results = []
     async with semaphore:
         async with aiohttp.ClientSession() as session:
             tasks = [fetch_url(session, url) for url in urls]
             for task in asyncio.as_completed(tasks):
-                url, res = await task
-                results[url] = res
+                res = await task
+                results.append(res)
+
+    if results:
+        await save_results(results, out_file)
     return results
 
 
-# async def main():
-#
-#     fetched_data = await fetch_urls(data)
+async def main():
+    input_file = "urls.txt"
+    output_file = "results.json"
+
+    if os.path.exists(output_file):
+        os.remove(output_file)
+
+    await fetch_urls(input_file, output_file)
 
 
-if __name__ == '__main__':
-    input_file = 'urls.txt'
-    output_file = 'result.json'
-    data = reads_urls(input_file)
-    fetched_data = asyncio.run(fetch_urls(data))
-    save_file(output_file, fetched_data)
+if __name__ == "__main__":
+    asyncio.run(main())
