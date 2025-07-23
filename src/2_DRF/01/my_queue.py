d3bc27d8c75a31e713709033a4f2a5a5a927e3bd
SDE <deanisik@gmail.com>
2025-07-23 11:52:44 +0700

    my queue

--- src/2_DRF/01/my_queue.py
+++ src/2_DRF/01/my_queue.py
@@ -1,4 +1,4 @@
-'''
+"""
 Задача - Очередь
 Реализуйте класс очереди который использует редис под капотом
 
@@ -19,7 +19,7 @@ if __name__ == '__main__':
     assert q.consume() == {'a': 1}
     assert q.consume() == {'b': 2}
     assert q.consume() == {'c': 3}
-'''
+"""
 
 import json
 
@@ -27,19 +27,18 @@ import redis
 
 
 class RedisQueue:
-
-    def __init__(self, host='localhost', port=6379, db=0, queue_name='default_queue'):
-        self.redis_client = redis.Redis(host=host, port=port, db=db, decode_responses=True)
+    def __init__(self, host="localhost", port=6379, db=0, queue_name="default_queue"):
+        self.redis_client = redis.Redis(
+            host=host, port=port, db=db, decode_responses=True
+        )
         self.queue_name = queue_name
-
 
     def publish(self, msg: dict):
         try:
             msg = json.dumps(msg)
-            self.redis_client.publish(self.queue_name, msg)
+            self.redis_client.lpush(self.queue_name, msg)
         except Exception as e:
             raise e
-
 
     def consume(self) -> dict:
         try:
@@ -50,12 +49,13 @@ class RedisQueue:
         except Exception as e:
             raise e
 
-if __name__ == '__main__':
-    q = RedisQueue()
-    q.publish({'a': 1})
-    q.publish({'b': 2})
-    q.publish({'c': 3})
 
-    assert q.consume() == {'a': 1}
-    assert q.consume() == {'b': 2}
-    assert q.consume() == {'c': 3}
+if __name__ == "__main__":
+    q = RedisQueue()
+    q.publish({"a": 1})
+    q.publish({"b": 2})
+    q.publish({"c": 3})
+
+    assert q.consume() == {"a": 1}
+    assert q.consume() == {"b": 2}
+    assert q.consume() == {"c": 3}
