4d68effaef42d9ea38f9621e39d2164e6dd3cd7a
SDE <deanisik@gmail.com>
2025-07-23 11:53:01 +0700

    rate limiter

--- src/2_DRF/01/rate_limiter.py
+++ src/2_DRF/01/rate_limiter.py
@@ -1,4 +1,4 @@
-'''
+"""
 Задача - Ограничитель скорости (rate limiter)
 Ваше приложение делает HTTP запросы в сторонний сервис (функция make_api_request), при этом сторонний сервис имеет
 проблемы с производительностью и ваша задача ограничить количество запросов к этому сервису - не больше пяти запросов
@@ -7,12 +7,13 @@
 возвращает True в случае если лимит на кол-во запросов не достигнут
 возвращает False если за последние 3 секунды уже сделано 5 запросов.
 Ваша реализация должна использовать Redis, т.к. предполагается что приложение работает на нескольких серверах.
-'''
+"""
 
 import random
 import time
 
 import redis
+import requests
 
 
 class RateLimitExceed(Exception):
@@ -20,13 +21,11 @@ class RateLimitExceed(Exception):
 
 
 class RateLimiter:
-
-    def __init__(self, redis_host: str = 'localhost', redis_port: int = 6379):
+    def __init__(self, redis_host: str = "localhost", redis_port: int = 6379):
         self.redis = redis.Redis(host=redis_host, port=redis_port, db=0)
-        self.key = 'rate_limiter'
+        self.key = "rate_limiter"
         self.time_limin = 3
         self.count = 5
-
 
     def test(self) -> bool:
         current_time = int(time.time())
@@ -48,21 +47,18 @@ class RateLimiter:
             return False
 
 
-
-
-
 def make_api_request(rate_limiter: RateLimiter):
     if not rate_limiter.test():
         raise RateLimitExceed
     else:
-        # какая-то бизнес логика
-        pass
+        request = requests.get("https://google.cm")
+        return request
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     rate_limiter = RateLimiter()
 
-    for _ in range(50):
+    for _ in range(20):
         time.sleep(random.randint(1, 2))
 
         try:
