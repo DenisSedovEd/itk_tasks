91fb299d7e7235c1fa538fe789d1905becd2bfda
SDE <deanisik@gmail.com>
2025-09-03 20:53:15 +0700

    two sum v2

--- src/3_tasks/algorithmic/two_sum.py
+++ src/3_tasks/algorithmic/two_sum.py
@@ -1,21 +1,35 @@
 # nums = [2, 7, 11, 15]
 # target = 9
 
-def two_sum(nums: list, target: int) -> list | str:
-    result = None
-    for idx, _ in enumerate(nums):
-        if idx == (len(nums)-1):
-            result = 'Такой пары нет'
-        for idy, _ in enumerate(nums):
+
+def two_sum_v1(nums: list, target: int) -> list:
+    nums_len = len(nums)
+    for idx in range(nums_len):
+        for idy in range(idx + 1, nums_len):
             if nums[idx] + nums[idy] == target:
                 return [idx, idy]
-    return result
-
-if __name__ == '__main__':
-    assert two_sum(nums=[2, 7, 11, 15], target=17) == [0, 3]
-    assert two_sum(nums=[2, 7, 11, 15], target=9) == [0, 1]
-    assert two_sum(nums=[2, 7, 11, 15], target=26) == [2, 3]
-    assert two_sum(nums=[2, 7, 11, 15], target=22) == [1, 3]
-    assert two_sum(nums=[2, 7, 11, 15], target=0) == 'Такой пары нет'
+    return []
 
 
+def two_sum_v2(nums: list, target: int) -> list:
+    res = {}
+    for idx, num in enumerate(nums):
+        subtraction = target - num
+        if subtraction in res:
+            return [res[subtraction], idx]
+        res[num] = idx
+    return []
+
+
+if __name__ == "__main__":
+    assert two_sum_v1(nums=[2, 7, 11, 15], target=17) == [0, 3]
+    assert two_sum_v1(nums=[2, 7, 11, 15], target=9) == [0, 1]
+    assert two_sum_v1(nums=[2, 7, 11, 15], target=26) == [2, 3]
+    assert two_sum_v1(nums=[2, 7, 11, 15], target=22) == [1, 3]
+    assert two_sum_v1(nums=[2, 7, 11, 15], target=0) == []
+
+    assert two_sum_v2(nums=[2, 7, 11, 15], target=17) == [0, 3]
+    assert two_sum_v2(nums=[2, 7, 11, 15], target=9) == [0, 1]
+    assert two_sum_v2(nums=[2, 7, 11, 15], target=26) == [2, 3]
+    assert two_sum_v2(nums=[2, 7, 11, 15], target=22) == [1, 3]
+    assert two_sum_v2(nums=[2, 7, 11, 15], target=0) == []
